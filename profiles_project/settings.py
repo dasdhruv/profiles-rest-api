a214cd0f0085539b099c0e8530722d3476da0d8e
Dhruv <maildhruvdas@gmail.com>
2020-06-18 16:36:11 +0530

    Ädded custom user model, superuser and migration

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Tells the django that look into the profiles_api and then the class UserProfile
+# To use the Authentication mechanism and user registration in our project
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
