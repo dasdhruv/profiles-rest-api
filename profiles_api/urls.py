3f33bf9267578bcfadbfd8acb4c53981555c515f
Dhruv <maildhruvdas@gmail.com>
2020-06-21 20:02:21 +0530

    Viewsets to your profiles_api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from profiles_api import views
+# Let us import a default routers for using viewsets
+from rest_framework.routers import DefaultRouter
+
+# To use viewsets
+router = DefaultRouter()
+router.register("Hello-ViewSet", views.HelloViewSets, base_name = "Hello-ViewSet")
 
 urlpatterns = [
     # as_view is the standard function that we call to convert our api view's class HelloApiView rended by the URLs
     # So, in our case it will call the function get if a HTTP get is requested
-    path('hello-view/', views.HelloApiView.as_view())
+    path('hello-view/', views.HelloApiView.as_view()),
+    # We are going to add a path for a blank string
+    # As you register new routes for the router object it generates a list of URLs which are associated for our viewsets
+    path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 # status module will contain the list of handy HTTP status codes that you can use when you return responds from API
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     # The serializer_class function comes with the class APIView that retrieves the configured serializes class for our view
@@ -11,7 +12,6 @@ class HelloApiView(APIView):
     def get(this, request, format = None):
         an_apiViews = ['Uses HTTP methods as function (get, put, post, delete, patch)','is similar to a traditional django view',
         'Gives you most control over your app logic','is mapped manually to your URLs']
-
         return Response({'message':"Hello World", 'an_apiViews':an_apiViews})
 
     def post(this, request):
@@ -34,3 +34,38 @@ class HelloApiView(APIView):
 
     def delete(this, request, pk = None):
         return Response({'method':'DELETE'})
+
+
+# To call the class http://127.0.0.1:8000/api/Hello-ViewSet/
+class HelloViewSets(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+    def list(this, request):
+        a_views_sets = ['Uses actions (list, create, retrieve, update, update partial)','Automatically maps to URLs using routers',
+        'Provides more functionality using less code']
+        return Response({'message':"Hello World", 'a_views_sets':a_views_sets})
+
+    def create(this, request):
+        # Create a new hello message
+        serializer = this.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            # The 'username' will be add a label to our web app
+            name = serializer.validated_data.get('username')
+            message = f'Hello {name}'
+            return Response({'post_message':message})
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    # To call the class http://127.0.0.1:8000/api/Hello-ViewSet/1
+    # 1 is the primary key
+    def retrieve(this, request, pk = None):
+        return Response({'http_method':'GET'})
+
+    def update(this, request, pk = None):
+        return Response({'http_method':'PUT'})
+
+    def partial_update(this, request, pk = None):
+        return Response({'http_method':'PATCH'})
+
+    def destroy(this, request, pk = None):
+        return Response({'http_method':'DELETE'})
